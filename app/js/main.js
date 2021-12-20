15703e59634fc25faef8a34377c67865d81157b2
Sihula <serhiisihula@gmail.com>
2021-12-20 20:52:25 +0100

    add adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -5,8 +5,46 @@ $(function () {
     dots: false,
     arrows: false,
     infinite: true,
-    autoplay: true
+    autoplay: true,
+    responsive: [{
+        breakpoint: 1180,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 980,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 680,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
 
 
+
+});
+
+let btn = document.querySelector('.burger'),
+  nav = document.querySelector('.header__nav');
+
+btn.addEventListener('click', () => {
+  nav.classList.toggle('active');
 });
