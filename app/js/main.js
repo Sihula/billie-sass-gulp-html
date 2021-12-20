4a46b9a51a5df0490486490277c97b869a815211
Sihula <serhiisihula@gmail.com>
2021-12-20 19:27:29 +0100

    add project

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+  $('.products__inner-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,
+    infinite: true,
+    autoplay: true
+  });
+
+
+});
